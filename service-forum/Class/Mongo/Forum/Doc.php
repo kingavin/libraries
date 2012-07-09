a53683adec1db8a7d1bbfbea7b9a71419c34c470
Gavin Li <craftgavin@gmail.com>
2012-07-09 12:30:36 +0800

    bug fixed

--- service-forum/Class/Mongo/Forum/Doc.php
+++ service-forum/Class/Mongo/Forum/Doc.php
@@ -1,5 +1,17 @@
 <?php
 class Class_Mongo_Forum_Doc extends App_Mongo_Db_Document
 {
-
+	protected $_field = array(
+			  "avatarcheck",
+  			  "avatartext",
+			  "captchacheck",
+			  "captchatext",
+			  "contenttext",
+			  "forumid",
+			  "logincheck",
+			  "logintext",
+			  "nametext",
+			  "orgCode",
+			  "titletext"			
+	);
 }
--- service-forum/Class/Mongo/Post/Doc.php
+++ service-forum/Class/Mongo/Post/Doc.php
@@ -1,5 +1,20 @@
 <?php
 class Class_Mongo_Post_Doc extends App_Mongo_Db_Document
 {
-
+	protected $_field = array(
+			"content",
+			"datatime",
+			"httpurl",
+			"isShow",
+			"lastDatatime",
+			"lastReply",
+			"lastReplyUsername",
+			"md5httpurl",
+			"orgCode",
+			"parentId",
+			"sort",
+			"status",
+			"title",
+			"username"				
+	);
 }
